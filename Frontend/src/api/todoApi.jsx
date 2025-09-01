46f9e3d92296dfe77f4cd902f5c0597cf8caa0d7
Dipika Gandash <dipikadevv1445@gmail.com>
2025-09-01 22:21:34 +0530

    Implement POST /create endpoint for adding todos

--- Frontend/src/api/todoApi.jsx
+++ Frontend/src/api/todoApi.jsx
@@ -2,11 +2,35 @@ import axios from "axios";
 
 const API_URL = "http://localhost:3000/todo";
 
-export const fetchAllTodos = async (params = {}) => {
-  const response =  axios.get(`${API_URL}/todos`, {
-    params,
-    withCredentials: true,
-    headers: { "Content-Type": "application/json" },
-  });
-  return response.data;
+export const fetchAllTodosApi = async (params = {}) => {
+  try {
+    const response = await axios.get(`${API_URL}/todos`, {
+      params,
+      withCredentials: true,
+      headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (err) {
+    if (err.response?.status === 401) {
+      throw new Error("You must be logged in to see your todos.");
+    } else {
+      throw new Error("Failed to Fetch todo.");
+    }
+  }
+};
+
+export const createTodoApi = async (todoData) => {
+  try {
+    const response = await axios.post(`${API_URL}/create`, todoData, {
+      withCredentials: true,
+      headers: { "Content-Type": "application/json" },
+    });
+    return response.data;
+  } catch (err) {
+    if (err.response?.status === 401) {
+      throw new Error("You must be logged in to create a todo.");
+    } else {
+      throw new Error("Failed to create todo.");
+    }
+  }
 };
--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,22 +1,24 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-import { fetchAllTodos } from "../api/todoApi";
+import React, { useEffect, useState } from "react";
+import { fetchAllTodosApi , createTodoApi} from "../api/todoApi";
 
 const Home = () => {
   const [todos, setTodos] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+  const [priority, setPriority] = useState("");
+  const [dueDate, setDueDate] = useState("");
 
   useEffect(() => {
     const fetchTodo = async () => {
       try {
         setLoading(true);
-        const response = await fetchAllTodos();
+        const response = await fetchAllTodosApi();
         if (response.success) {
-          setTodos(response.todos || []);
+          setTodos(response.todos);
+          console.log(response)
         } else {
-          setError(response.todos.message);
+          setError(response.message || "Failed to fetch todos");
         }
       } catch (error) {
         setError(error.message || "Something went wrong");
@@ -24,14 +26,48 @@ const Home = () => {
         setLoading(false);
       }
     };
+
     fetchTodo();
   }, []);
+
+  const todoCreate = async () => {
+    if(!newTodo.trim()) return ;
+    setLoading(true);
+    setError(null);
+    try{
+      
+      const todoData = {
+        title: newTodo
+      };
+
+      if (priority.trim() !== "") todoData.priority = priority;
+if (dueDate.trim() !== "") todoData.dueDate = dueDate;
+
+      const response = await createTodoApi(todoData);
+
+      if(response.success){
+        setTodos((prevTodos) => [...prevTodos, response.todo]);
+        setNewTodo("");
+        setPriority("");
+        setDueDate("");
+      }
+      else{
+        setError(response.message || "Failed to create todo.");
+      }
+    }catch (err) {
+      setError(err.message);
+    } finally{
+      setLoading(false);
+    }
+  }
+
   return (
     <div>
       <h2>My Todos</h2>
-     
     </div>
-  )
+  );
 };
+
+
 
 export default Home;
